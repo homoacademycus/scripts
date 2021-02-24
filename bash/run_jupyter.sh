193a6282a3a46bcd080515ad672cf314856e45a3
programmer <programmer@develop.net>
2021-02-24 14:04:40 +0900

    2021-02-24

---
+++ bash/run_jupyter.sh
@@ -0,0 +1,1 @@
+jupyter lab --ip 172.17.0.0 --port  --no-browser &
--- bash/tool_docker_compose.sh
+++ bash/tool_docker_compose.sh
@@ -5,7 +5,8 @@
 #
 echo "please locate this script file under dir with docker-compose file"
 path_current_dir=$(dirname -- "$(readlink -f -- "${BASH_SOURCE[0]}")")
-echo "this script is exected under dir : $path_current_dir"
+echo -n "---->> this script is exected under dir : $path_current_dir"
+ls .
 echo -n "set docker compose file name(example.yaml) :"
 read filename
 echo -n "set log level(debug for develop, info for production) :"
@@ -21,11 +22,12 @@ echo -n "
 3. down all services on stack
 4. down certain service on stack
 5. stop and remove all docker containers
+6. build certain service on stack
 **********************************
---> enter number(0~5) : "
+--> enter number(0~6) : "
 read op
 
-#docker rmi $(docker images dev-* -q)
+cat $path_current_dir/$filename
 
 case $op in
 0)
@@ -38,6 +40,7 @@ case $op in
     ;;
 2)
     echo -n "set service name on docker-compose stack :"
+    read certainServiceName
     docker-compose --log-level $level --file $filename up $certainServiceName -d 2>&1
     docker ps -a
     ;;
@@ -47,6 +50,7 @@ case $op in
     ;;
 4)
     echo -n "set service name on docker-compose stack :"
+    read certainServiceName
     docker-compose --file $filename down $certainServiceName
     docker ps -a
     ;;
@@ -55,6 +59,11 @@ case $op in
     docker container stop $(docker ps -aq)
     docker container rm $(docker ps -aq)
     docker ps -aq
+    ;;
+6)
+    echo -n "set service name on docker-compose stack :"
+    read certainServiceName
+    docker-compose --file $filename build --parallel $certainServiceName
     ;;
 *)
   echo "please enter to continue.."
