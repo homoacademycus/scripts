2849b5b14d4704e03978d3157b04cbafdfddb241
programmer <programmer@develop.net>
2021-03-02 20:10:11 +0900

    2021-03-02/fixed bugs:envar name, added postgre related files

--- bash/install_react.sh
+++ bash/install_react.sh
@@ -1,10 +1,10 @@
 npm init -y
 RUN npm install -g create-react-app
-npm install -g --save-dev eslint prettier eslint-config-prettier
+npm install -g --save-dev eslint prettier eslint-config-prettier eslint-plugin-react gh-pages
 npm install -g react react-dom react-router-dom react-app react-scripts 
-npm install -g styled-components prop-types
-npm install -g axios react-redux redux-actions redux-sega
-npm install brcypt jsonwebtoken axios gh-pages
+npm install -g styled-components prop-types react-table
+npm install -g react-redux redux-actions redux-sega
+npm install -g axios qs brcypt jsonwebtoken faker cra-template
 eslint --init
 
 #export PORT=8888 | echo $PORT
